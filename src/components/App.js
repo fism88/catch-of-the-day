ab5fd342a1f073a65e40e2faf4d9aad363e86c49
Faraaz Ismail <fism88@gmail.com>
2018-01-27 15:23:47 -0600

    Add PropType validations on components; add basic social authentications for store owners

--- src/components/App.js
+++ src/components/App.js
@@ -111,12 +111,17 @@ class App extends React.Component {
           addFish={this.addFish}
           loadSamples={this.loadSamples}
           fishes={this.state.fishes}
-          updateFish={this.updateFish}
+          storeId={this.props.params.storeId}
           removeFish={this.removeFish}
+          updateFish={this.updateFish}
         />
       </div>
     )
   }
 }
+
+App.propTypes = {
+  params: React.PropTypes.object.isRequired
+};
 
 export default App;
--- src/components/Inventory.js
+++ src/components/Inventory.js
@@ -1,13 +1,31 @@
 import React from 'react';
 
 import AddFishForm from './AddFishForm';
+import base from '../base';
 
 class Inventory extends React.Component {
   constructor () {
     super();
 
+    this.authenticate = this.authenticate.bind(this);
+    this.authHandler = this.authHandler.bind(this);
     this.handleChange = this.handleChange.bind(this);
+    this.logout = this.logout.bind(this);
     this.renderInventory = this.renderInventory.bind(this);
+    this.renderLogin = this.renderLogin.bind(this);
+
+    this.state = {
+      uid: null,
+      owner: null
+    }
+  }
+
+  componentDidMount () {
+    base.onAuth((user) => {
+      if (user) {
+        this.authHandler(null, { user });
+      }
+    });
   }
 
   handleChange (event, key) {
@@ -18,6 +36,49 @@ class Inventory extends React.Component {
     }
 
     this.props.updateFish(key, updatedFish);
+  }
+
+  authenticate (provider) {
+    base.authWithOAuthPopup(provider, this.authHandler);
+  }
+
+  authHandler (err, authData) {
+    if (err) {
+      console.error(err);
+      return;
+    }
+
+    const storeRef = base.database().ref(this.props.storeId);
+    storeRef.once('value', (snapshot) => {
+      const data = snapshot.val() || {};
+
+      if (!data.owner) {
+        storeRef.set({
+          owner: authData.user.uid
+        });
+      }
+      this.setState({
+        uid: authData.user.uid,
+        owner: data.owner || authData.user.uid
+      });
+    });
+  }
+
+  logout () {
+    base.unauth();
+    this.setState({ uid: null });
+  }
+
+  renderLogin () {
+    return (
+      <nav className="login">
+        <h2>Inventory</h2>
+        <p>Sign in to manage your store's inventory</p>
+        <button className="github" onClick={() => this.authenticate('github')}>Log In with Github</button>
+        <button className="facebook" onClick={() => this.authenticate('facebook')}>Log In with Facebook</button>
+        <button className="twitter" onClick={() => this.authenticate('twitter')}>Log In with Twitter</button>
+      </nav>
+    )
   }
 
   renderInventory (key) {
@@ -39,9 +100,24 @@ class Inventory extends React.Component {
   }
 
   render () {
+    const logout = <button onClick={() => this.logout()}>Log Out</button>;
+    if (!this.state.uid) {
+      return <div>{this.renderLogin()}</div>
+    }
+
+    if (this.state.uid !== this.state.owner) {
+      return (
+        <div>
+          <p>Sorry, you aren't the owner of this store.</p>
+          {logout}
+        </div>
+      )
+    }
+
     return (
       <div>
         <h2>Inventory</h2>
+        {logout}
         <AddFishForm addFish={ this.props.addFish }/>
         <button onClick={this.props.loadSamples}>Load Sample Fishes</button>
         <br/><br/>
@@ -50,5 +126,14 @@ class Inventory extends React.Component {
     )
   }
 }
+
+Inventory.propTypes = {
+  addFish: React.PropTypes.func.isRequired,
+  fishes: React.PropTypes.object.isRequired,
+  loadSamples: React.PropTypes.func.isRequired,
+  removeFish: React.PropTypes.func.isRequired,
+  storeId: React.PropTypes.string.isRequired,
+  updateFish: React.PropTypes.func.isRequired
+};
 
 export default Inventory;
